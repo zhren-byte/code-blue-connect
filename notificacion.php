803aecf93cc90958ba441c48383e9c520c5bb806
zhren-byte <chavez.thomas970@gmail.com>
2023-10-03 15:58:40 -0300

    Notificaciones Mobile

--- notificacion.php
+++ notificacion.php
@@ -1,5 +1,7 @@
 <?php 
     include('./connection.php');
-    $notificacion = $conn->query("SELECT * FROM notificaciones ORDER BY date DESC, time DESC LIMIT 1;")->fetch_assoc();
-    echo $notificacion['time'];
+    $notificacion = $conn->query("SELECT notificaciones.time, ubicaciones.name as origen, zonas.name as zona FROM notificaciones INNER JOIN ubicaciones ON ubicaciones.id = notificaciones.ubi_id INNER JOIN zonas ON zonas.id = ubicaciones.zone_id WHERE status_id = 0 ORDER BY date DESC, time DESC LIMIT 1;")->fetch_assoc();
+    $time = date('G:i', strtotime($notificacion['time']));
+    $array = array("time" => $time, "origen" => $notificacion['origen'], "zona" => $notificacion['zona']);
+    echo json_encode($array);
     
